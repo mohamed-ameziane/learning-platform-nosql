4022445c5251d9764f1fdc31d27cea1b51b419b8
Mohamed Ameziane <med.ameez02@gmail.com>
2025-01-13 01:32:49 +0100

    Ajout de fonctions utilitaires pour la recherche et l'insertion de documents dans MongoDB

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -1,14 +1,37 @@
 // Question: Pourquoi créer des services séparés ?
-// Réponse: 
+// Réponse: Pour séparer les responsabilités et avoir l'aspect de modularité.
 
 const { ObjectId } = require('mongodb');
 
 // Fonctions utilitaires pour MongoDB
 async function findOneById(collection, id) {
   // TODO: Implémenter une fonction générique de recherche par ID
+  try {
+    if (!ObjectId.isValid(id)) {
+      throw new Error("Invalid ID format");
+    }
+    const objectId = ObjectId.createFromHexString(id);
+    const result = await collection.findOne({ _id: objectId });
+    return result;
+  } catch (error) {
+    console.error("Error finding document by ID:", error);
+    throw error;
+  }
+}
+
+async function insertOne(collection, data) {
+  try {
+    const result = await collection.insertOne(data);
+    return result;
+  } catch (error) {
+    console.error("Error inserting document:", error);
+    throw error;
+  }
 }
 
 // Export des services
 module.exports = {
   // TODO: Exporter les fonctions utilitaires
+  findOneById,
+  insertOne,
 };
