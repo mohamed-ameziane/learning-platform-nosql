9d4ffb08ba710eb7b57f3ca57932e7e26bf37330
Mohamed Ameziane <med.ameez02@gmail.com>
2025-01-16 13:33:13 +0100

    Ajout de la connexion Redis et de la fonction de mise en cache des données

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -1,13 +1,33 @@
 // Question : Comment gérer efficacement le cache avec Redis ?
-// Réponse :
+// Réponse : En utilisant des clés uniques et des valeurs JSON.
 // Question: Quelles sont les bonnes pratiques pour les clés Redis ?
-// Réponse :
+// Réponse : Utiliser des clés uniques et des valeurs JSON.
+
+const redis = require("redis");
+const client = redis.createClient({ url: process.env.REDIS_URI });
+
+client.on("error", (err) => console.error("Redis Client Error", err));
+
+async function connectRedis() {
+  if (!client.isOpen) {
+    await client.connect();
+  }
+}
 
 // Fonctions utilitaires pour Redis
 async function cacheData(key, data, ttl) {
-    // TODO: Implémenter une fonction générique de cache
+  // TODO: Implémenter une fonction générique de cache
+  try {
+    await client.set(key, JSON.stringify(data), {
+      EX: ttl
+    });
+    console.log(`Data cached with key: ${key}`);
+  } catch (error) {
+    console.error('Error caching data:', error);
   }
+}
   
   module.exports = {
     // TODO: Exporter les fonctions utilitaires
+    cacheData
   };
